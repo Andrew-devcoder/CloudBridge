9e4fef4970746107eb395fa0ad95394943f306a4
andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-03-15 13:07:12 +0100

    test

--- src/conf/redis.config.ts
+++ src/conf/redis.config.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 export const redisClient = createClient({
-  username: 'default',
+  username: 'database-cloudinary',
   password: process.env.REDIS_PASSWORD,
   socket: {
     host: process.env.REDIS_HOST,
