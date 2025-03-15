9e8a9683abca8e01e320c34c5fa654bf90427009
andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-03-15 18:22:27 +0100

    fix

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
+@Global()
 @Module({
   imports: [AppModule],
   controllers: [AppController],
--- src/modules/cloudinary.module.ts
+++ src/modules/cloudinary.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CloudinaryController } from 'src/controllers/cloudinary.controller';
 import { CloudinaryService } from 'src/services/cloudinary.service';
 
+@Global()
 @Module({
   imports: [CloudinaryModule],
   controllers: [CloudinaryController],
