e2f45e7ac045755836b0d9e9f078b1d010fbe102
andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-03-15 18:43:24 +0100

    fix

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { CloudinaryModule } from './cloudinary.module';
+import { RedisModule } from './redis.module';
 
+@Global()
 @Module({
-  imports: [],
+  imports: [CloudinaryModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [AppService],
 })
 export class AppModule {}
