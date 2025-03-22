c56603f42555005a656dd520ec15ac2aa561953a
andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-03-22 15:17:58 +0100

    test rabbit get

--- src/controllers/rabbit.controller.ts
+++ src/controllers/rabbit.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { RabbitMQService } from '../services/rabbit.service';
 
 @Controller('messages')
 export class RabbitMQController {
   constructor(private readonly rabbitMQService: RabbitMQService) {}
 
-  @Post('send')
+  @Get('send')
   sendMessage(@Body('message') message: string): string {
     this.rabbitMQService.sendMessage(message);
     return `Message sent: ${message}`;
