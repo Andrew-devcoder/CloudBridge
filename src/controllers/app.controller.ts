df853982291fb1a648438e2f9b9d40ce11cdc7dd
andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-03-22 20:02:35 +0100

    fix favicon

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get('')
   async getStatus(): Promise<object> {
     return this.appService.getStatus();
+  }
+
+  @Get('favicon.ico')
+  getFavicon(@Res() res: Response) {
+    res.status(204).end();
   }
 }
